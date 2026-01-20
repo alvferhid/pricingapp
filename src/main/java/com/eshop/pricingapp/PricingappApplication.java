c43f57393a51e1eb169bf03c8f22c39cdc482320
Alvaro <alvferhid@gmail.com>
2026-01-20 13:18:31 +0100

    Initial commit

---
+++ src/main/java/com/eshop/pricingapp/PricingappApplication.java
@@ -0,0 +1,13 @@
+package com.eshop.pricingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricingappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eshop/pricingapp/PricingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshop.pricingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
